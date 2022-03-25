b5841e873c0ef2420c3f5a5aa19e0d25c3f50c0b
Tobias Noebauer <tnobauer@rockefeller.edu>
2022-03-25 12:51:44 -0400

    Update wisim_arduino_stream.py
    Minor logging

--- wisim_arduino_stream.py
+++ wisim_arduino_stream.py
@@ -13,7 +13,7 @@ else:
 
 try:
 	logfile_handle = False
-	ser = serial.Serial('COM6', 115200, timeout=0.1)
+	ser = serial.Serial('COM5', 115200, timeout=0.1)
 	ser.write(b'r')  # reset arduino to "waiting for trigger" state
 	#filename = input("Enter output file name base: ")
 	logfile_handle = open(filename + '.txt', 'w')
@@ -26,6 +26,7 @@ try:
 				if ch == b'q':
 					break
 				elif ch == b'r':
+					print('Sending reset command')
 					ser.write(b'r')
 
 			if ser.in_waiting == 0:
